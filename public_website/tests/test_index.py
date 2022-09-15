f2bc3ded618992a86696f5c9676d507773c75763
mjeammet <marie.jeammet@protonmail.com>
2022-09-15 10:29:48 +0200

    removed empty context and separate route-view and view-template test

--- public_website/tests/test_index.py
+++ public_website/tests/test_index.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
-
+from django.urls import resolve, reverse
+from public_website import views
 
 class TestIndex(TestCase):
 
-    def test_index_url_triggers_the_right_template(self):
-        response = self.client.get("/")
+    def test_index_url_calls_right_view(self):
+        resolved = resolve(reverse('index'))
+        self.assertEqual(resolved.func, views.index_view)
+
+    def test_index_view_calls_right_template(self):
+        response = self.client.get(reverse('index'))
         self.assertTemplateUsed(response, "public_website/index.html")
 
     def test_index_response_contains_welcome_message(self):
--- public_website/views.py
+++ public_website/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 
 def index_view(request):
-    context = {}
-    return render(request, 'public_website/index.html', context)
+    return render(request, 'public_website/index.html')
