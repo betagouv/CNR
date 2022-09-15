704041fc57b9cbc8540c585454a186f15073e6b4
mjeammet <marie.jeammet@protonmail.com>
2022-09-15 10:46:51 +0200

    modified tests on index url-view-template

--- public_website/tests/test_index.py
+++ public_website/tests/test_index.py
@@ -5,11 +5,11 @@ from public_website import views
 class TestIndex(TestCase):
 
     def test_index_url_calls_right_view(self):
-        resolved = resolve(reverse('index'))
-        self.assertEqual(resolved.func, views.index_view)
+        match = resolve('/')
+        self.assertEqual(match.func, views.index_view)
 
-    def test_index_view_calls_right_template(self):
-        response = self.client.get(reverse('index'))
+    def test_index_url_calls_right_template(self):
+        response = self.client.get('/')
         self.assertTemplateUsed(response, "public_website/index.html")
 
     def test_index_response_contains_welcome_message(self):
