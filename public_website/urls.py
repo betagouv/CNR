7fbbb1d1db6d67d6e124a298e4a07ffd9c28f484
Lisa Durand <lisa.c.durand@gmail.com>
2022-09-16 16:03:33 +0200

    add fonctionnement page - view url and links

--- public_website/urls.py
+++ public_website/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index'),
+    path('fonctionnement', views.fonctionnement_view, name='fonctionnement'),
 ]
--- public_website/views.py
+++ public_website/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'public_website/index.html')
+
+def fonctionnement_view(request):
+    return render(request, 'public_website/fonctionnement.html')
