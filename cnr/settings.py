662d766ed359be32a14d4b0d0e3095f51e2a7a0b
BenoitSerrano <benoit.serrano10@gmail.com>
2022-09-16 14:41:03 +0200

    add allowed host

--- cnr/settings.py
+++ cnr/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1', os.getenv('HOST_URL', 'localhost')]
 
 
 # Application definition
