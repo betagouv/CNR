ea2c876ebb4759668682da873ed80a0a07267f17
mjeammet <marie.jeammet@protonmail.com>
2022-09-17 00:23:31 +0200

    Create Participant and Subscription models
    Co-authored-by: mjeammet <marie.jeammet@beta.gouv.fr>
    Co-authored-by: Anna-Livia Gomart <anna-livia.gomart@gmail.com>

--- public_website/models.py
+++ public_website/models.py
@@ -1,3 +1,38 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Theme(models.TextChoices):
+    EDUCATION = "EDUCATION", "Éducation"
+    SANTE = "SANTE", "Santé"
+
+
+class ParticipantType(models.TextChoices):
+    CITOYEN = "CITOYEN", "Citoyen"
+    ELU = "ELU", "Élu"
+    ASSOCIATION = "ASSOCIATION", "Représentant d'une association"
+
+
+class Participant(models.Model):
+    email = models.EmailField(
+        unique=True, verbose_name="Courriel", blank=False, null=False
+    )
+    first_name = models.CharField(
+        max_length=100, verbose_name="Prénom", blank=False, null=False
+    )
+    postal_code = models.CharField(
+        max_length=10, verbose_name="Code Postal", blank=False, null=False
+    )
+    participant_type = models.CharField(
+        blank=False,
+        null=False,
+        choices=ParticipantType.choices,
+        max_length=30,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Subscription(models.Model):
+    participant = models.ForeignKey(Participant, models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    theme = models.CharField(blank=False, choices=Theme.choices, max_length=30)
