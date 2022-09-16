9984bf55c7f214f968879bc9f74e6b8f4a5cd1f8
mjeammet <marie.jeammet@protonmail.com>
2022-09-17 00:23:31 +0200

    Change template to get the right fields and error messages
    Co-authored-by: mjeammet <marie.jeammet@beta.gouv.fr>
    Co-authored-by: Anna-Livia Gomart <anna-livia.gomart@gmail.com>

--- public_website/views.py
+++ public_website/views.py
@@ -1,28 +1,29 @@
-from django.shortcuts import render
-from public_website.forms import InscriptionForm
-from django.http import HttpResponseRedirect
-from django.shortcuts import redirect
 from django.contrib import messages
+from django.shortcuts import render
+
+from public_website.forms import InscriptionForm
 
 
 def index_view(request):
-    return render(request, 'public_website/index.html')
+    return render(request, "public_website/index.html")
 
 
 def inscription_view(request):
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = InscriptionForm(request.POST)
         if form.is_valid():
+            form.save()
             # Send information to sendinblue
-            thank_you_message = 'Données enregistrées. Merci pour votre intérêt !'
+            thank_you_message = "Données enregistrées. Merci pour votre intérêt !"
             messages.success(request, thank_you_message)
         else:
-            # Bad Request ? 
-            error_message = "Formulaire invalide. Veuillez vérifier vos réponses."            
+            error_message = "Formulaire invalide. Veuillez vérifier vos réponses."
             messages.error(request, error_message)
-    if request.method == 'GET':
+
+    if request.method == "GET":
         form = InscriptionForm()
-    return render(request, "public_website/inscription.html", {'form': form})
+    return render(request, "public_website/inscription.html", {"form": form})
 
 
 def fonctionnement_view(request):
