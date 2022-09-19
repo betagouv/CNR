6d575b2aad52c6ab6f2d499dddf3b98b154a112c
BenoitSerrano <benoit.serrano10@gmail.com>
2022-09-19 16:06:06 +0200

    disable debug mode in production

--- cnr/settings.py
+++ cnr/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv("IS_DEBUG_ENABLED", "False") == "True" else False
 
 ALLOWED_HOSTS = (
     os.getenv("HOST_URL", "127.0.0.1, localhost").replace(" ", "").split(",")
