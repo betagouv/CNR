e8ed0dcbbf223b8a253aec690cd3dac380e20315
Lisa Durand <lisa.c.durand@gmail.com>
2022-09-20 17:09:24 +0200

    add pages relative to survey

--- public_website/urls.py
+++ public_website/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path("mentions-legales/", views.mentions_legales_view, name="mentions_legales"),
     path("accessibilite/", views.accessibilite_view, name="accessibilite"),
     path("donnees-personnelles/", views.donnees_personnelles_view, name="donnees_personnelles"),
+    path("survey/", views.survey_view, name="survey"),
+    path("survey-intro/", views.survey_intro_view, name="survey_intro"),
+    path("survey-outro/", views.survey_outro_view, name="survey_outro"),
 ]
