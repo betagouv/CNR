8a723942a3c2f1be9f350822927797734308854b
Anna-Livia Gomart <anna-livia.gomart@beta.gouv.fr>
2022-09-27 14:38:32 +0200

    Add themes to SIB

--- public_website/email_provider.py
+++ public_website/email_provider.py
@@ -1,9 +1,8 @@
 import logging
-import os
 
 import sib_api_v3_sdk
+from django.conf import settings
 from dotenv import load_dotenv
-from sib_api_v3_sdk.rest import ApiException
 
 from public_website.models import Participant, Theme
 
@@ -13,17 +12,20 @@ logger = logging.getLogger(__name__)
 
 def send_payload_to_send_in_blue(email: str, payload: dict) -> bool:
     configuration = sib_api_v3_sdk.Configuration()
-    configuration.api_key["api-key"] = os.getenv("SEND_IN_BLUE")
+    configuration.api_key["api-key"] = settings.SEND_IN_BLUE
     try:
         api_instance = sib_api_v3_sdk.ContactsApi(
             sib_api_v3_sdk.ApiClient(configuration)
         )
         contact = sib_api_v3_sdk.CreateContact(
-            email=email, update_enabled=True, attributes=payload, list_ids=[1]
+            email=email,
+            update_enabled=True,
+            attributes=payload,
+            list_ids=[settings.SEND_IN_BLUE_LIST],
         )
         api_instance.create_contact(contact)
         return True
-    except ApiException as e:
+    except sib_api_v3_sdk.rest.ApiException as e:
         logger.exception("Exception when calling ContactsApi->create_contact: %s", e)
         raise
 
@@ -40,10 +42,17 @@ def create_payload_for_email_provider(participant: Participant):
     subscription_list = participant.get_subscription_list()
     return {
         "PRENOM": participant.first_name,
-        "THEME_SANTE": Theme.SANTE in subscription_list,
-        "THEME_EDUCATION": Theme.EDUCATION in subscription_list,
         "PARTICIPANT_TYPE": participant_type,
         "CODE_POSTAL": participant.postal_code,
+        "THEME_SANTE": Theme.SANTE in subscription_list,
+        "THEME_EDUCATION": Theme.EDUCATION in subscription_list,
+        "THEME_CLIMAT": Theme.CLIMAT in subscription_list,
+        "THEME_VIEILLISSEMENT": Theme.VIEILLISSEMENT in subscription_list,
+        "THEME_SOUVERAINETE": Theme.SOUVERAINETE in subscription_list,
+        "THEME_TRAVAIL": Theme.TRAVAIL in subscription_list,
+        "THEME_LOGEMENT": Theme.LOGEMENT in subscription_list,
+        "THEME_JEUNESSE": Theme.JEUNESSE in subscription_list,
+        "THEME_NUMERIQUE": Theme.NUMERIQUE in subscription_list,
     }
 
 
