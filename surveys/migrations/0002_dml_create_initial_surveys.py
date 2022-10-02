449d1661580d598ab47199ba4a50710b16ff36c6
Anna-Livia Gomart <anna-livia.gomart@beta.gouv.fr>
2022-10-02 09:59:48 +0200

    enhance the migration of fixtures

--- surveys/migrations/0002_dml_create_initial_surveys.py
+++ surveys/migrations/0002_dml_create_initial_surveys.py
@@ -11,8 +11,9 @@ from public_website.models import Theme
 def populate_surveys(apps, schema_editor):
     Survey = apps.get_model("surveys", "Survey")
     SurveyQuestion = apps.get_model("surveys", "SurveyQuestion")
+    themes = [theme for theme in Theme if theme not in ["EDUCATION", "SANTE"]]
 
-    for theme in Theme:
+    for theme in themes:
         Survey.objects.create(
             label=f"{theme.name}_1",
             hr_label=theme.label,
@@ -25,17 +26,20 @@ def populate_surveys(apps, schema_editor):
     with open(path) as file:
         data = csv.reader(file, delimiter=";")
         for row in data:
+
             survey_label = f"{row[0]}"
 
             survey = Survey.objects.get(label=survey_label)
             for i in range(1, len(row)):
-                if i in (1, 2):
+                if "les trois mots" in row[i]:
                     answer_type = "THREE_TEXT_FIELD"
-                if i == 3:
+                elif "chantiers prioritaires" in row[i]:
                     answer_type = "FIVE_TEXT_FIELD"
-                if i == 4:
+                elif "une première proposition" in row[i]:
                     answer_type = "ONE_TEXT_FIELD"
-
+                else:
+                    print(survey_label, row[i])
+                    raise Exception
                 SurveyQuestion.objects.create(
                     survey=survey,
                     label=f"{survey_label}_Q_{i}",
