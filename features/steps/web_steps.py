be4e53e182e5861d0eb23cfb815a961052694ef3
Denis B <denis.baudot@gmail.com>
2022-10-10 15:16:45 +0200

    Add meaningfull names for behave steps functions

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -6,47 +6,40 @@ from selenium.webdriver.common.by import By
 # The Selenium API could be better in its interface so add a bit of
 # plumbing here to help us design sane, reusable steps
 
-"""
-Find a label containing LABEL on the page
-"""
-
 
 def find_label(context, label):
+    """
+    Find a label containing LABEL on the page
+    """
     return context.browser.find_element(
         By.XPATH, '//label[contains(.,"{}")]'.format(label)
     )
 
 
-"""
-Find a checkbox by its label on the page
-"""
-
-
 def find_checkbox_label(context, label):
+    """
+    Find a checkbox by its label on the page
+    """
     return context.browser.find_element(
         By.XPATH, '//label[contains(.,"{}")]/span'.format(label)
     )
 
 
-"""
-Find a label matching LABEL on the page, click to focus its input
-and send the VALUE down into the field.
-"""
-
-
 def fill_input_with_label(context, label, value):
+    """
+    Find a label matching LABEL on the page, click to focus its input
+    and send the VALUE down into the field.
+    """
     label = find_label(context, label).click()
 
     context.browser.switch_to.active_element.send_keys(value)
 
 
-"""
-Find a fieldset whose legend matches LEGEND and find the first label
-within containing VALUE in its text.
-"""
-
-
 def find_option_with_label(context, legend, value):
+    """
+    Find a fieldset whose legend matches LEGEND and find the first label
+    within containing VALUE in its text.
+    """
     return context.browser.find_element(
         By.XPATH,
         '//fieldset/legend[contains(., "{}")]/..//label[contains(., "{}")]'.format(
@@ -56,53 +49,45 @@ def find_option_with_label(context, legend, value):
 
 
 @when("je me rends sur la page d'accueil")
-def step_impl1(context):
+def step_go_to_homepage(context):
     context.browser.get(context.get_url("/"))
 
 
 @then('je peux lire "{}" dans la page')
-def step_impl2(context, text):
+def step_read_page_contains(context, text):
     assert text in context.browser.find_element(By.TAG_NAME, "main").text
 
 
 @then('la page est titrée "{}"')
-def step_impl3(context, title):
+def step_read_page_title(context, title):
     assert context.browser.title == title
 
 
 @when('je remplis le champ "{}" avec "{}"')
-def step_impl4(context, label, value):
+def step_fill_field(context, label, value):
     fill_input_with_label(context, label, value)
 
 
 @when('je coche la case "{}"')
-def step_impl5(context, label):
+def step_check_box(context, label):
     find_checkbox_label(context, label).click()
 
 
 @when('je choisis "{}" pour "{}"')
-def step_impl6(context, value, legend):
+def step_choose_option(context, value, legend):
     find_option_with_label(context, legend, value).click()
 
 
-@when('je sélectionne "{}" pour "{}"')
-def step_impl7(context, options_list, legend):
-    options = options_list.split(", ")
-
-    for option in options:
-        find_option_with_label(context, legend, option).click()
-
-
 @when('je clique sur "{}"')
-def step_impl8(context, label):
+def step_click_on_option(context, label):
     context.browser.find_element(By.LINK_TEXT, label).click()
 
 
 @when("je soumets le formulaire")
-def step_impl9(context):
+def step_submit_form(context):
     context.browser.find_element(By.XPATH, '//form//*[@type="submit"]').click()
 
 
 @then('un email a été envoyé à "{}"')
-def step_impl10(context, email):
+def step_send_email(context, email):
     context.mocks["sib"].assert_called_with(email, payload=ANY)
