721b55c6ffddb4b54d58f6c0e6b36416a2f27936
mjeammet <marie.jeammet@protonmail.com>
2022-10-14 11:25:07 +0200

    add test partial profile

--- public_website/tests/test_models.py
+++ public_website/tests/test_models.py
@@ -11,3 +11,16 @@ class ParticipantModelTest(TestCase):
     def test_has_profile(self):
         self.assertTrue(self.participant.has_profile)
         self.assertFalse(self.no_profile_participant.has_profile)
+
+    def test_partial_profile_is_considered_empty(self):
+        participant = NoProfileParticipantFactory()
+        self.assertFalse(participant.has_profile)
+
+        participant.postal_code = "33750"
+        participant.save()
+        self.assertFalse(participant.has_profile)
+
+        participant.first_name = "Prénom"
+        participant.participant_type = "Particulier"
+        participant.save()
+        self.assertTrue(participant.has_profile)
